2800f00deb4ffef7435fe64837631e0816969060
Rahul Tiwari <rahultiwari161189@gmail.com>
2023-08-03 23:36:05 +0530

    did server side coding for for uploading question

--- Faculties/uploadQuestions.php
+++ Faculties/uploadQuestions.php
@@ -7,7 +7,103 @@
     $class = "";
     $sub = "";
     $weightage = "";
+    $option1 = "";
+    $option2 = "";
+    $option3 = "";
+    $option4 = "";
+    $options = "";  
     $questionErr="";
+    $typeErr = "";
+    $levelErr = "";
+    $classErr = "";
+    $subErr = "";
+    $weightageErr = "";
+    $optionErr = "";
+    $successfull = false;
+
+
+    if(isset($_POST["addhojabhai"]) && ($_POST["addhojabhai"])=="Add"){
+        $question = $_POST["question"] ;
+        $type = $_POST["type"];
+        $level = $_POST["level"];
+        $class = $_POST["class"];
+        $sub = $_POST["sub"];
+        $weightage = $_POST["weightage"];
+
+        $err = false;
+
+        if(trim($question) == ""){
+            $questionErr = "Please Enter a Question *";
+            $err = true;
+        }
+        if(trim($type) == ""){
+            $typeErr = "Please Select Question Type *";
+            $err = true;
+        }
+        if(trim($level) == ""){
+            $levelErr = "Please Select Question Level *";
+            $err = true;
+        }
+        if(trim($class) == ""){
+            $classErr = "Please Select Class *";
+            $err = true;
+        }
+        if(trim($sub) == ""){
+            $subErr = "Please Select a Subject *";
+            $err = true;
+        }
+        if(trim($weightage) == ""){
+            $weightageErr = "Please Enter Weightage of Question *";
+            $err = true;
+        }
+        if(trim($type)=="mcqs"){
+            $option1 = $_POST["opt1"];
+            $option2 = $_POST["opt2"];
+            $option3 = $_POST["opt3"];
+            $option4 = $_POST["opt4"];
+
+            if(trim($option1)=="" || trim($option2)==""  || trim($option3)==""  || trim($option4)=="" ){
+                $err = true;
+                $optionErr = "Please Enter all the Options.! *";
+            }
+            $options = $option1 ." , ".$option2 ." , ".$option3 ." , ".$option4;
+        }
+
+        if(!$err){
+            $question = htmlspecialchars($question);
+            $currentDate = date("Y-m-d");
+            $sql = "insert into tbl_questions values(NULL,'$question','$type','$options','$level',$weightage,1,1,1,'$currentDate')";
+            if($conn->query($sql) === TRUE){
+                $successfull = true;
+                $question = "" ;
+    $type="";
+    $level = "";
+    $class = "";
+    $sub = "";
+    $weightage = "";
+    $option1 = "";
+    $option2 = "";
+    $option3 = "";
+    $option4 = "";
+    $questionErr="";
+    $typeErr = "";
+    $levelErr = "";
+    $classErr = "";
+    $subErr = "";
+    $weightageErr = "";
+    $optionErr = "";
+            }
+            else{
+                $successfull = false;
+            }
+        }
+
+
+        
+        
+
+    }
+
 ?>
 <html lang="en">
 <head>
@@ -21,13 +117,22 @@
     <?
         include("../Partials/navbar.php");
     ?>
+    <?php
+    if($successfull){
+        echo "
+            <section class='p-[3vw] w-[100vw]  bg-green-500 absolute top-0 shadow-xl'>
+                <p class='flex justify-center items-center'> Question added Successfully</p>
+            </section>
+        ";
+    }
+    ?>
 
     <main class="mt-5 ">
         <section class="flex justify-center items-center ">
         
             <form action="uploadQuestions.php" method="post" class="bg-white shadow-xl rounded-xl p-10">
                 <div class="my-2">
-                    <select name="type" id="type" class="block shadow-xl appearance-none w-full py-2 px-4 pr-8 rounded-lg border focus:outline-none focus:ring focus:border-blue-300">
+                    <select name="type" id="type" class="block shadow-xl appearance-none w-full py-2 px-4 pr-8 rounded-lg border focus:outline-none focus:ring focus:border-blue-300" required>
                         <div class="bg-white p-2">
                             <option value=""  <?php if($type=="") echo "selected";?>> ------- Select a Question Type ----------</option>
                             <option value="mcqs" <?php if($type=="mcqs") echo "selected";?>> MCQS </option>
@@ -36,9 +141,14 @@
                             <option value="atf" <?php if($type=="atf") echo "selected";?>> Answer The Following Question. </option>
                         </div>
                     </select>
+                    <?php
+                        if($typeErr){
+                            echo "<p class='text-red-500 my-3 '> $typeErr </p>";
+                        }
+                    ?>
                 </div>
                 <div class="my-2">
-                    <textarea name="question" placeholder="Enter your Question"  class="block shadow-xl appearance-none w-full py-2 px-4 pr-8 rounded-lg border focus:outline-none focus:ring focus:border-blue-300 resize-none  shadow-lg h-32 w-[80vw] lg:w-[50vw]" value="<?php echo $question;?>"></textarea>
+                    <textarea name="question" required placeholder="Enter your Question"  class="block shadow-xl appearance-none w-full py-2 px-4 pr-8 rounded-lg border focus:outline-none focus:ring focus:border-blue-300 resize-none  shadow-lg h-32 w-[80vw] lg:w-[50vw]" value=""><?php echo htmlspecialchars($question);?></textarea>
                     <?php
                         if($questionErr){
                             echo "<p class='text-red-500 my-3 '> $questionErr </p>";
@@ -47,12 +157,20 @@
                 </div>
                 <div class="my-2" id="option-div">
                         <input type="text" name="opt1"  placeholder="Option1" class="block shadow-xl  my-1  appearance-none w-full py-2 px-4 pr-8 rounded-lg border focus:outline-none focus:ring focus:border-blue-300"> 
+                      
                         <input type="text" name="opt2" placeholder="Option2" class="block shadow-xl my-1  appearance-none w-full py-2 px-4 pr-8 rounded-lg border focus:outline-none focus:ring focus:border-blue-300">
+                      
                         <input type="text" name="opt3" placeholder="Option3" class="block shadow-xl my-1  appearance-none w-full py-2 px-4 pr-8 rounded-lg border focus:outline-none focus:ring focus:border-blue-300">
+                      
                         <input type="text" name="opt4" placeholder="Option4" class="block shadow-xl  my-1 appearance-none w-full py-2 px-4 pr-8 rounded-lg border focus:outline-none focus:ring focus:border-blue-300">
+                        <?php
+                        if($optionErr){
+                            echo "<p class='text-red-500 my-3 '> $optionErr </p>";
+                        }
+                    ?>
                 </div>
                 <div class="my-2">
-                    <select name="level" id="level" class="block shadow-xl appearance-none w-full py-2 px-4 pr-8 rounded-lg border focus:outline-none focus:ring focus:border-blue-300">
+                    <select name="level" required id="level" class="block shadow-xl appearance-none w-full py-2 px-4 pr-8 rounded-lg border focus:outline-none focus:ring focus:border-blue-300">
                         <div class="bg-white my-2">
                             <option value=""  <?php if($level=="") echo "selected"; ?>>-------- Select Level of Your Question -----------</option>
                             <option value="easy" <?php if($level=="easy") echo "selected"; ?>>Easy</option>
@@ -60,12 +178,22 @@
                             <option value="hard" <?php if($level=="hard") echo "selected"; ?>>Hard</option>
                         </div>
                     </select>
+                    <?php
+                        if($levelErr){
+                            echo "<p class='text-red-500 my-3 '> $levelErr </p>";
+                        }
+                    ?>
                 </div>
                 <div class="my-2">
-                    <input type="number" name="weightage" value="<?php echo $weightage;?>" class="block shadow-xl  my-1 appearance-none w-full py-2 px-4 pr-8 rounded-lg border focus:outline-none focus:ring focus:border-blue-300" placeholder="Weightage ">
+                    <input type="number" required name="weightage" value="<?php echo $weightage;?>" class="block shadow-xl  my-1 appearance-none w-full py-2 px-4 pr-8 rounded-lg border focus:outline-none focus:ring focus:border-blue-300" placeholder="Weightage ">
+                    <?php
+                        if($weightageErr){
+                            echo "<p class='text-red-500 my-3 '> $weightageErr </p>";
+                        }
+                    ?>
                 </div>
                 <div class="my-2">
-                    <select name="class" id="class" class="block shadow-xl appearance-none w-full py-2 px-4 pr-8 rounded-lg border focus:outline-none focus:ring focus:border-blue-300">
+                    <select name="class" required id="class" class="block shadow-xl appearance-none w-full py-2 px-4 pr-8 rounded-lg border focus:outline-none focus:ring focus:border-blue-300">
                         <div class="bg-white my-2">
                             <option value=""  <?php if($class=="") echo "selected"; ?>>-------- Select Class -----------</option>
                             <option value="fy"    <?php if($class=="fy") echo "selected"; ?>>FY</option>
@@ -73,27 +201,42 @@
                             <option value="ty"   <?php if($class=="ty") echo "selected"; ?>>TY</option>
                         </div>
                     </select>
+                    <?php
+                        if($classErr){
+                            echo "<p class='text-red-500 my-3 '> $classErr </p>";
+                        }
+                    ?>
                 </div>
                 <div class="my-2">
-                    <select name="sub" id="sub" class="block shadow-xl appearance-none w-full py-2 px-4 pr-8 rounded-lg border focus:outline-none focus:ring focus:border-blue-300">
+                    <select name="sub" value="<?php echo $sub;?>" required id="sub" class="block shadow-xl appearance-none w-full py-2 px-4 pr-8 rounded-lg border focus:outline-none focus:ring focus:border-blue-300">
                         <div class="bg-white my-2">
                             <option value=""   <?php if($sub=="") echo "selected"; ?>>-------- Select Subject -----------</option>
-                            <option value="501">AWD</option>
-                            <option value="502">Network Technology</option>
-                            <option value="503">WFS</option>
-                            <option value="504">ASP.Net</option>
-                            <option value="505">Unix</option>
+                            <option value="501"  <?php if($sub=="501") echo "selected"; ?> >AWD</option>
+                            <option value="502"  <?php if($sub=="502") echo "selected"; ?>>Network Technology</option>
+                            <option value="503"  <?php if($sub=="503") echo "selected"; ?>>WFS</option>
+                            <option value="504"  <?php if($sub=="504") echo "selected"; ?>>ASP.Net</option>
+                            <option value="505"  <?php if($sub=="505") echo "selected"; ?>>Unix</option>
                         </div>
                     </select>
+                    <?php
+                        if($subErr){
+                            echo "<p class='text-red-500 my-3 '> $subErr </p>";
+                        }
+                    ?>
                 </div>
-                <input type="submit" value="Add" class="px-5 w-full py-2 bg-white text-red-500 border border-red-500 hover:bg-red-500 hover:text-white rounded-lg shadow-xl">
+                <input type="submit" name="addhojabhai" value="Add" class="px-5 w-full py-2 bg-white text-red-500 border border-red-500 hover:bg-red-500 hover:text-white rounded-lg shadow-xl">
             </form>
         </section>
     </main>
 
     <script type="text/javascript">
         $(document).ready(()=>{
-            $("#option-div").hide();
+
+                $("#option-div").hide();
+            if($("#type").val()=="mcqs"){
+                $("#option-div").show();
+
+            }
             $("#type").change(()=>{
                 if($("#type").val() == "mcqs"){
                     $("#option-div").show();
